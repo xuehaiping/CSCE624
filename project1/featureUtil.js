1549da295facc1ea3edce549e0c46a393a101bbf
xuehaiping <xuehaiping@users.noreply.github.com>
2017-10-02 20:38:20 -0500

    remove the training

---
+++ project1/featureUtil.js
@@ -0,0 +1,430 @@
+var test = {
+  "_id": {
+    "$oid": "59c333a093302e89254b7961"
+  },
+  "id": "2c2f3f3e-9b8f-4488-abe1-aaacf1234d57",
+  "time": "1351018915925",
+  "domain": ["SingleShapes", "SketchRec2017"],
+  "points": [{
+    "x": 757,
+    "y": 233,
+    "time": "1351018532736",
+    "id": "00000000-a13c-414d-8004-2106b4391974"
+  }, {
+    "x": 757,
+    "y": 232,
+    "time": "1351018532771",
+    "id": "00000000-a13d-414d-8004-2106b6d11155"
+  }, {
+    "x": 761,
+    "y": 230,
+    "time": "1351018532813",
+    "id": "00000000-a13e-414d-8004-2106b81faf5a"
+  }, {
+    "x": 766,
+    "y": 230,
+    "time": "1351018532829",
+    "id": "00000000-a13f-414d-8004-2106b963f48e"
+  }, {
+    "x": 776,
+    "y": 230,
+    "time": "1351018532851",
+    "id": "00000000-a140-414d-8004-2106ba8ffb71"
+  }, {
+    "x": 784,
+    "y": 230,
+    "time": "1351018532867",
+    "id": "00000000-a141-414d-8004-2106bbbc4998"
+  }, {
+    "x": 805,
+    "y": 230,
+    "time": "1351018532891",
+    "id": "00000000-a142-414d-8004-2106bd02c47e"
+  }, {
+    "x": 833,
+    "y": 230,
+    "time": "1351018532915",
+    "id": "00000000-a143-414d-8004-2106be528b78"
+  }, {
+    "x": 854,
+    "y": 230,
+    "time": "1351018532940",
+    "id": "00000000-a144-414d-8004-2106bfb479c2"
+  }, {
+    "x": 864,
+    "y": 231,
+    "time": "1351018532955",
+    "id": "00000000-a145-414d-8004-2106c101d122"
+  }, {
+    "x": 871,
+    "y": 231,
+    "time": "1351018532979",
+    "id": "00000000-a146-414d-8004-2106c24cb5f0"
+  }, {
+    "x": 872,
+    "y": 231,
+    "time": "1351018532998",
+    "id": "00000000-a147-414d-8004-2106c3960b56"
+  }, {
+    "x": 872,
+    "y": 231,
+    "time": "1351018533205",
+    "id": "00000000-a148-414d-8004-2106cff46d57"
+  }],
+  "strokes": [{
+    "time": "1351018533205",
+    "points": ["00000000-a13c-414d-8004-2106b4391974", "00000000-a13d-414d-8004-2106b6d11155", "00000000-a13e-414d-8004-2106b81faf5a", "00000000-a13f-414d-8004-2106b963f48e", "00000000-a140-414d-8004-2106ba8ffb71", "00000000-a141-414d-8004-2106bbbc4998", "00000000-a142-414d-8004-2106bd02c47e", "00000000-a143-414d-8004-2106be528b78", "00000000-a144-414d-8004-2106bfb479c2", "00000000-a145-414d-8004-2106c101d122", "00000000-a146-414d-8004-2106c24cb5f0", "00000000-a147-414d-8004-2106c3960b56", "00000000-a148-414d-8004-2106cff46d57"],
+    "id": "00000000-a15e-414d-8004-2106cff658cc"
+  }],
+  "substrokes": [{
+        "time": "1351018533205",
+        "points": ["00000000-a13c-414d-8004-2106b4391974", "00000000-a13d-414d-8004-2106b6d11155", "00000000-a13e-414d-8004-2106b81faf5a", "00000000-a13f-414d-8004-2106b963f48e", "00000000-a140-414d-8004-2106ba8ffb71", "00000000-a141-414d-8004-2106bbbc4998", "00000000-a142-414d-8004-2106bd02c47e", "00000000-a143-414d-8004-2106be528b78", "00000000-a144-414d-8004-2106bfb479c2", "00000000-a145-414d-8004-2106c101d122", "00000000-a146-414d-8004-2106c24cb5f0", "00000000-a147-414d-8004-2106c3960b56", "00000000-a148-414d-8004-2106cff46d57"],
+        "id": "00000000-a15e-414d-8004-2106cff658cc"
+      }, {
+        "time": "1351018533753",
+        "points": ["00000000-a36f-414d-8004-2106e0abb181", "00000000-a370-414d-8004-2106e60feedf", "00000000-a371-414d-8004-2106e76d3c51", "00000000-a372-414d-8004-2106e8bfb2bd", "00000000-a373-414d-8004-2106ea1ccc38", "00000000-a374-414d-8004-2106ecc19275", "00000000-a375-414d-8004-2106f0ccd013"],
+        "id": "00000000-a385-414d-8004-2106f0ce4c2c"
+      }, {
+        "time": "1351018534446",
+        "points": ["00000000-a706-414d-8004-21070e75f9bf", "00000000-a707-414d-8004-2107129db013", "00000000-a708-414d-8004-2107140dd586", "00000000-a709-414d-8004-210715707778", "00000000-a70a-414d-8004-210716cc0600","00000000-a70b-414d-8004-2107182e9621","00000000-a70c-414d-8004-21071af70d9d"],"id":"00000000-a71c-414d-8004-21071af89a0c"}],"shapes":[{"subElements":["00000000-a15e-414d-8004-2106cff658cc","00000000-a385-414d-8004-2106f0ce4c2c","00000000-a71c-414d-8004-21071af89a0c"],"time":"1351018534446","interpretation":"arrow","confidence":"1.0"}]};
+
+function arrayToObj(array) {
+      var obj = {}
+      for (var i = 0; i < array.length; i++) {
+          var val = array[i];
+          var id = val.id;
+          delete val.id;
+          obj[id] = val;
+      }
+      return obj;
+  }
+
+test.pointsDict = arrayToObj(test.points);
+
+/* Helper functions */
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
+
+function squareEuclideanDistance(p1, p2) {
+  var deltaX = p1["x"] - p2["x"];
+  var deltaY = p1["y"] - p2["y"];
+  return deltaX*deltaX + deltaY*deltaY;
+}
+
+function euclideanDistance(p1, p2) {
+  return Math.sqrt(squareEuclideanDistance(p1, p2));
+}
+
+function cosine(p1, p2) {
+  var numerator = p2["x"] - p1["x"];
+  var denominator = euclideanDistance(p1, p2);
+  if(denominator == 0) return 0;
+  return numerator/denominator;
+}
+
+function sine(p1, p2) {
+  var numerator = p2["y"] - p1["y"];
+  var denominator = euclideanDistance(p1, p2);
+  if(denominator == 0) return 0;
+  return numerator/denominator;
+}
+
+function maxPoint(points) {
+  var maxX = Number.MIN_VALUE;
+  var maxY = Number.MIN_VALUE;
+  for(var i = 0; i < points.length; i++) {
+    if(points[i]["x"] > maxX) maxX = points[i]["x"];
+    if(points[i]["y"] > maxY) maxY = points[i]["y"];
+  }
+  return {x:maxX, y:maxY};
+}
+
+function minPoint(points) {
+  var minX = Number.MAX_VALUE;
+  var minY = Number.MAX_VALUE;
+  for(var i = 0; i < points.length; i++) {
+    if(points[i]["x"] < minX) minX = points[i]["x"];
+    if(points[i]["y"] < minY) minY = points[i]["y"];
+  }
+  return {x:minX, y:minY};
+}
+/* Start of features */
+
+function initAngleCosine(sketch) {
+  if(typeof sketch["points"][0] === "undefined" || typeof sketch["points"][0] === "undefined") {
+      console.log(sketch);
+  }
+  //return cosine(sketch.points[sketch.strokes[0].points[0]], sketch.points[sketch.strokes[0].points[2]]);
+  return cosine(sketch["points"][0], sketch["points"][2]);
+}
+
+function initAngleSine(sketch) {
+  return sine(sketch["points"][0], sketch["points"][2]);
+}
+
+function BoundBoxDiagonalLength(sketch) {
+  var maxP = maxPoint(sketch["points"]);
+  var minP = minPoint(sketch["points"]);
+  return euclideanDistance(maxP, minP);
+}
+
+function BoundBoxDiagonalAngle(sketch) {
+  var maxP = maxPoint(sketch["points"]);
+  var minP = minPoint(sketch["points"]);
+  var numerator = maxP["y"] - minP["y"];
+  var denominator = maxP["x"] - minP["x"];
+  if(denominator == 0) return 0;
+  return Math.atan(numerator/denominator);
+}
+
+function firstLastPointDistance(sketch) {
+  var len = sketch["points"].length;
+  return euclideanDistance(sketch["points"][0], sketch["points"][len-1]);
+}
+
+function firstLastPointCosine(sketch) {
+  var len = sketch["points"].length;
+  return cosine(sketch["points"][0], sketch["points"][len-1]);
+}
+
+function firstLastPointSine(sketch) {
+  var len = sketch["points"].length;
+  return sine(sketch["points"][0], sketch["points"][len-1]);
+}
+
+function totalGestureLength(sketch) {
+  var sum = 0;
+  for(var i = 0; i < sketch["strokes"].length; i++) {
+    var stroke = sketch["strokes"][i]["points"];
+    for(var j = 0; j < stroke.length-1; j++) {
+      var p1 = sketch.pointsDict[stroke[j]];
+      var p2 = sketch.pointsDict[stroke[j+1]];
+      if(typeof p1 === "undefined" || typeof p2 === "undefined") {
+          continue;
+      }
+      sum += euclideanDistance(p1, p2);
+    }
+  }
+  return sum;
+}
+
+// function totalGestureLength_test(sketch) {
+//   var sum = 0;
+//   for(var i = 0; i < sketch["points"].length-1; i++) {
+//     sum += euclideanDistance(sketch["points"][i], sketch["points"][i+1]);
+//   }
+//   return sum;
+// }
+
+//definition Theta(p) in paper
+function thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1) {
+  var numerator = deltaX*deltaYMinus1 - deltaXMinus1*deltaY;
+  var denominator = deltaX*deltaXMinus1 + deltaY*deltaYMinus1;
+  if(denominator == 0) return 0;
+  return Math.atan(numerator/denominator);
+}
+
+function totalAngleTraversed(sketch) {
+  var angleSum = 0;
+
+  for(var i = 0; i < sketch["strokes"].length; i++) {
+    var stroke = sketch["strokes"][i]["points"];
+    for(var j = 1; j < stroke.length-1; j++) {
+      if(typeof sketch.pointsDict[stroke[j+1]] === "undefined" || typeof sketch.pointsDict[stroke[j]] === "undefined" || typeof sketch.pointsDict[stroke[j-1]] === "undefined") {
+          continue;
+      }
+      var deltaX = sketch.pointsDict[stroke[j+1]]["x"]-sketch.pointsDict[stroke[j]]["x"],
+      deltaY = sketch.pointsDict[stroke[j+1]]["y"]-sketch.pointsDict[stroke[j]]["y"],
+      deltaXMinus1 = sketch.pointsDict[stroke[j]]["x"]-sketch.pointsDict[stroke[j-1]]["x"],
+      deltaYMinus1 = sketch.pointsDict[stroke[j]]["y"]-sketch.pointsDict[stroke[j-1]]["y"];
+      angleSum += thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1);
+    }
+  }
+  return angleSum;
+}
+//
+// function totalAngleTraversed_test(sketch) {
+//   var angleSum = 0;
+//   var points = sketch["points"];
+//   for(var i =1; i < points.length-1; i++) {
+//     if(typeof sketch.pointsDict[stroke[j+1]] === "undefined" || typeof sketch.pointsDict[stroke[j]] === "undefined" || typeof sketch.pointsDict[stroke[j-1]]) {
+//         continue;
+//     }
+//     var deltaX = points[i+1]["x"]-points[i]["x"], deltaY = points[i+1]["y"]-points[i]["y"],
+//     deltaXMinus1 = points[i]["x"]-points[i-1]["x"], deltaYMinus1 = points[i]["y"]-points[i-1]["y"];
+//     angleSum += thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1);
+//   }
+//   return angleSum;
+// }
+
+function absoluteTotalAngleTraversed(sketch) {
+  var absAngleSum = 0;
+  for(var i = 0; i < sketch["strokes"].length; i++) {
+    var stroke = sketch["strokes"][i]["points"];
+    for(var j = 1; j < stroke.length-1; j++) {
+      if(typeof sketch.pointsDict[stroke[j+1]] === "undefined" || typeof sketch.pointsDict[stroke[j]] === "undefined" || typeof sketch.pointsDict[stroke[j-1]] === "undefined") {
+          continue;
+      }
+      var deltaX = sketch.pointsDict[stroke[j+1]]["x"]-sketch.pointsDict[stroke[j]]["x"],
+      deltaY = sketch.pointsDict[stroke[j+1]]["y"]-sketch.pointsDict[stroke[j]]["y"],
+      deltaXMinus1 = sketch.pointsDict[stroke[j]]["x"]-sketch.pointsDict[stroke[j-1]]["x"],
+      deltaYMinus1 = sketch.pointsDict[stroke[j]]["y"]-sketch.pointsDict[stroke[j-1]]["y"];
+      absAngleSum += Math.abs(thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1));
+    }
+  }
+
+  return absAngleSum;
+}
+
+// function absoluteTotalAngleTraversed_test(sketch) {
+//   var absAngleSum = 0;
+//   var points = sketch["points"];
+//   for(var i =1; i < points.length-1; i++) {
+//     var deltaX = points[i+1]["x"]-points[i]["x"], deltaY = points[i+1]["y"]-points[i]["y"],
+//     deltaXMinus1 = points[i]["x"]-points[i-1]["x"], deltaYMinus1 = points[i]["y"]-points[i-1]["y"];
+//     absAngleSum += Math.abs(thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1));
+//   }
+//   console.log("test " + absAngleSum);
+//   return absAngleSum;
+// }
+
+function squareTotalAngleTraversed(sketch) {
+  var angleSquareSum = 0;
+  for(var i = 0; i < sketch["strokes"].length; i++) {
+    var stroke = sketch["strokes"][i]["points"];
+    for(var j = 1; j < stroke.length-1; j++) {
+      if(typeof sketch.pointsDict[stroke[j+1]] === "undefined" || typeof sketch.pointsDict[stroke[j]] === "undefined" || typeof sketch.pointsDict[stroke[j-1]] === "undefined") {
+          continue;
+      }
+      var deltaX = sketch.pointsDict[stroke[j+1]]["x"]-sketch.pointsDict[stroke[j]]["x"],
+      deltaY = sketch.pointsDict[stroke[j+1]]["y"]-sketch.pointsDict[stroke[j]]["y"],
+      deltaXMinus1 = sketch.pointsDict[stroke[j]]["x"]-sketch.pointsDict[stroke[j-1]]["x"],
+      deltaYMinus1 = sketch.pointsDict[stroke[j]]["y"]-sketch.pointsDict[stroke[j-1]]["y"];
+      angleSquareSum += Math.pow(thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1), 2);
+    }
+  }
+  return angleSquareSum;
+}
+
+// function squareTotalAngleTraversed_test(sketch) {
+//   var angleSquareSum = 0;
+//   var points = sketch["points"];
+//   for(var i =1; i < points.length-1; i++) {
+//     var deltaX = points[i+1]["x"]-points[i]["x"], deltaY = points[i+1]["y"]-points[i]["y"],
+//     deltaXMinus1 = points[i]["x"]-points[i-1]["x"], deltaYMinus1 = points[i]["y"]-points[i-1]["y"];
+//     angleSquareSum += Math.pow(thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1), 2);
+//   }
+//   return angleSquareSum;
+// }
+
+function maxSpeedSquare(sketch) {
+  var maxDelta = Number.MIN_VALUE;
+  for(var i = 0; i < sketch["strokes"].length; i++) {
+    var stroke = sketch["strokes"][i]["points"];
+    for(var j = 0; j < stroke.length-1; j++) {
+      var p1 = sketch.pointsDict[stroke[j]];
+      var p2 = sketch.pointsDict[stroke[j+1]];
+      if(typeof sketch.pointsDict[stroke[j+1]] === "undefined" || typeof sketch.pointsDict[stroke[j]] === "undefined") {
+          continue;
+      }
+      var deltaTime = p2["time"] - p1["time"];
+      if(deltaTime != 0) {
+        var div = squareEuclideanDistance(p1, p2) / (deltaTime * deltaTime);
+        maxDelta = Math.max(div, maxDelta);
+      }
+    }
+  }
+  return maxDelta;
+}
+
+// function maxSpeedSquare_test(sketch) {
+//   var maxDelta = Number.MIN_VALUE;
+//   var points = sketch["points"];
+//   for(var i = 0; i < points.length-1; i++) {
+//     var deltaTime = points[i+1]["time"] - points[i]["time"];
+//     if(deltaTime != 0) {
+//       var div = squareEuclideanDistance(points[i], points[i+1]) / (deltaTime * deltaTime);
+//       maxDelta = Math.max(div, maxDelta);
+//     }
+//   }
+//   return maxDelta;
+// }
+
+function gestureDuration(sketch) {
+  var points = sketch["points"];
+  return points[points.length-1]["time"] - points[0]["time"];
+}
+
+function aspectRatio(sketch) {
+  return Math.abs(toRadians(45) - BoundBoxDiagonalAngle(sketch));
+}
+
+function Curviness(sketch) {
+  for(var i = 0; i < sketch["strokes"].length; i++) {
+    var angle19 = toRadians(19);
+    var absAngleSum = 0;
+    var stroke = sketch["strokes"][i]["points"];
+    for(var j = 1; j < stroke.length-1; j++) {
+      if(typeof sketch.pointsDict[stroke[j+1]] === "undefined" || typeof sketch.pointsDict[stroke[j]] === "undefined" || typeof sketch.pointsDict[stroke[j-1]]=== "undefined") {
+          continue;
+      }
+      var deltaX = sketch.pointsDict[stroke[j+1]]["x"]-sketch.pointsDict[stroke[j]]["x"],
+      deltaY = sketch.pointsDict[stroke[j+1]]["y"]-sketch.pointsDict[stroke[j]]["y"],
+      deltaXMinus1 = sketch.pointsDict[stroke[j]]["x"]-sketch.pointsDict[stroke[j-1]]["x"],
+      deltaYMinus1 = sketch.pointsDict[stroke[j]]["y"]-sketch.pointsDict[stroke[j-1]]["y"];
+        var curAngle = Math.abs(thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1));
+        if(curAngle < angle19)
+          absAngleSum += curAngle;
+    }
+  }
+  return absAngleSum;
+}
+
+// function Curviness_test(sketch) {
+//   var angle19 = toRadians(19);
+//   var absAngleSum = 0;
+//   var points = sketch["points"];
+//   for(var i =1; i < points.length-1; i++) {
+//     var deltaX = points[i+1]["x"]-points[i]["x"], deltaY = points[i+1]["y"]-points[i]["y"],
+//     deltaXMinus1 = points[i]["x"]-points[i-1]["x"], deltaYMinus1 = points[i]["y"]-points[i-1]["y"];
+//     var curAngle = thetaP(deltaX, deltaXMinus1, deltaY, deltaYMinus1);
+//     if(curAngle < angle19)
+//       absAngleSum += Math.abs(curAngle);
+//   }
+//   return absAngleSum;
+// }
+
+function strokeLenEndPointRatio(sketch) {
+  return totalGestureLength(sketch)/firstLastPointDistance(sketch);
+}
+
+function strokeLenBoundingBoxRatio(sketch) {
+  return totalGestureLength(sketch)/BoundBoxDiagonalLength(sketch);
+}
+
+function openess(sketch) {
+  return firstLastPointDistance(sketch)/BoundBoxDiagonalLength(sketch);
+}
+
+function areaOfBoundingBox(sketch) {
+  var maxP = maxPoint(sketch["points"]);
+  var minP = minPoint(sketch["points"]);
+  return Math.abs((maxP.x - minP.x) * (maxP.y - minP.y));
+}
+
+function logAreaOfBoundingBox(sketch) {
+  return Math.log10(areaOfBoundingBox(sketch));
+}
+
+function rotationalChange(sketch) {
+  return totalAngleTraversed(sketch)/absoluteTotalAngleTraversed(sketch);
+}
+
+function logStrokeLen(sketch) {
+  return Math.log10(totalGestureLength(sketch));
+}
+
+function logAspectRatio(sketch) {
+  return Math.log10(aspectRatio(sketch));
+}
